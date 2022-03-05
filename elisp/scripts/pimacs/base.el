d7a956b61515cf3e0d04e6f2f23b4910eb44ec42
Federico Tedin <federicotedin@gmail.com>
2022-03-06 00:42:37 +0100

    Remove Elisp repl

--- elisp/scripts/pimacs/base.el
+++ elisp/scripts/pimacs/base.el
@@ -3,10 +3,5 @@
 
   (fset 'run-me (function (lambda () (prin1 "Hello!\n" t))))
 
-  ;; (while t
-  ;;   (set 'exp (read))
-  ;;   (set 'res (eval exp))
-  ;;   (princ res t)
-  ;;   (princ "\n" t))
 
   )
