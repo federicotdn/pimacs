68316eacd48fbb9d623dc1f0f3257ce9e43d5d10
Federico Tedin <federicotedin@gmail.com>
2023-03-05 15:15:19 +0100

    Parse args too

--- tools/extract.py
+++ tools/extract.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 from collections import namedtuple
 import os
-from pyparsing import Literal, Regex, QuotedString, SkipTo, Opt
+from pyparsing import Literal, Regex, QuotedString, SkipTo, Opt, delimited_list
 
+FILE_HEADER = """// Automatically generated with pimacs.extract
+// DO NOT MODIFY!
+
+"""
+AUTOGEN_SUFFIX = "_autogen"
 GO_KEYWORDS = [
     "break",
     "default",
@@ -30,6 +35,10 @@ GO_KEYWORDS = [
     "return",
     "var",
 ]
+EMACS_CONSTANTS = {
+    "coding_arg_max": 13,
+    "charset_arg_max": 17,
+}
 
 DEFUN_EXPR_IDENTIFIER = Regex(r"[0-9a-zA-Z_-]+")
 DEFUN_EXPR_NUMBER = Regex(r"[0-9]+")
@@ -56,10 +65,24 @@ DEFUN_EXPR = (
     + "*/"
     + Opt("attributes:" + (Literal("noreturn") | "const")("attributes"))
     + ")"
+    + Opt(
+        "("
+        + (
+            delimited_list(
+                Opt("register") + "Lisp_Object" + DEFUN_EXPR_IDENTIFIER("args*"), min=1
+            )
+            | "void"
+            | Literal("ptrdiff_t nargs, Lisp_Object *args")
+        )
+        + ")"
+    )
 )
 
+GoDefun = namedtuple("GoDefun", "subr_fn numargs minargs lname fnname args")
+GoConstant = namedtuple("GoConstant", "name value")
 
-def extract_symbols(path: Path) -> tuple:
+
+def extract_declarations(path: Path) -> tuple:
     with open(path) as f:
         contents = f.read()
 
@@ -83,8 +106,18 @@ def extract_symbols(path: Path) -> tuple:
             current_defun.clear()
 
             parsed = DEFUN_EXPR.parse_string(defun)
-            result = dict(parsed)
-            result["file"] = path.stem
+            result = {
+                "lname": parsed["lname"],
+                "fnname": parsed["fnname"],
+                "sname": parsed["sname"],
+                "minargs": parsed["minargs"],
+                "maxargs": parsed["maxargs"],
+                "intspec": parsed["intspec"],
+                "args": parsed.get("args"),
+                "attributes": parsed.get("attributes"),
+                "doc": parsed["doc"].strip(),
+            }
+
             defuns.append(result)
         elif current_defun:
             current_defun.append(line)
@@ -104,16 +137,16 @@ def to_camel_case(text: str) -> str:
 
 
 def fnname_to_go(fnname: str) -> str:
-    name = to_camel_case(fnname[1:])
-    if name in GO_KEYWORDS:
-        name += "_"
-    return name + "_autogen"
+    return to_camel_case(fnname[1:]) + AUTOGEN_SUFFIX
 
 
-def generate_go_file(defuns: list[dict]) -> str:
-    GoDefun = namedtuple("GoDefun", "subr_fn numargs minargs lname fnname")
+def go_safe_identifier(identifier: str) -> str:
+    return identifier if identifier not in GO_KEYWORDS else identifier + "_"
+
+
+def get_go_declarations(defuns: list[dict]) -> tuple:
     go_defuns = {}
-    s = "package elisp\n\n"
+    go_constants = []
 
     for defun in defuns:
         lname = defun["lname"]
@@ -138,29 +171,41 @@ def generate_go_file(defuns: list[dict]) -> str:
         try:
             minargs = int(defun["minargs"])
         except ValueError:
-            minargs = "emacsConstant_" + defun["minargs"]
+            minargs = "emacsConstant_" + defun["minargs"] + AUTOGEN_SUFFIX
+            go_constants.append(GoConstant(minargs, str(EMACS_CONSTANTS[defun["minargs"]])))
 
         fnname = fnname_to_go(defun["fnname"])
-        go_defun = GoDefun(subr_fn, numargs, minargs, lname, fnname)
+        args = None
+        if defun["args"] is not None:
+            args = [go_safe_identifier(arg) for arg in defun["args"]]
+
+        go_defun = GoDefun(subr_fn, numargs, minargs, lname, fnname, args)
 
         if lname in go_defuns:
-            # if hash(go_defuns[lname]) != hash(go_defun):
-            #     raise Exception(
-            #         f"Duplicated defun with different properties",
-            #         go_defun,
-            #         go_defuns[lname],
-            #     )
-
+            # TODO: Fix repeated defuns
             continue
 
         go_defuns[lname] = go_defun
+
+    return go_defuns, go_constants
+
+
+def generate_go_file(defuns: list[dict]) -> str:
+    go_defuns, go_constants = get_go_declarations(defuns)
+    s = FILE_HEADER + "package elisp\n\n"
+
+    for constant in go_constants:
+        s += f"const {constant.name} = {constant.value}\n"
+
+    if go_constants:
+        s += "\n"
 
     for defun in go_defuns.values():
         s += f"func (ec *execContext) {defun.fnname}("
         if defun.subr_fn == "defSubrM":
             s += "args ...lispObject"
         elif defun.subr_fn != "defSubr0":
-            s += ", ".join([f"arg{i}" for i in range(defun.numargs)]) + " lispObject"
+            s += ", ".join(defun.args) + " lispObject"
         s += ") (lispObject, error) {\n"
         s += "	return ec.nil_, nil\n"
         s += "}\n\n"
@@ -179,19 +224,21 @@ def generate_go_file(defuns: list[dict]) -> str:
 
 
 def main() -> None:
-    base = Path(os.environ["EMACS_REPO_ROOT"])
+    emacs_base = Path(os.environ["EMACS_REPO_ROOT"])
+    pimacs_base = Path(os.environ["PIMACS_REPO_ROOT"])
 
     defuns = []
 
-    for p in base.joinpath("src").rglob("*"):
+    for p in emacs_base.joinpath("src").rglob("*"):
         if not p.suffix == ".c" or p.name == "msdos.c":
             continue
 
-        file_defuns, *_ = extract_symbols(p)
+        file_defuns, *_ = extract_declarations(p)
         defuns.extend(file_defuns)
 
-    f = generate_go_file(defuns)
-    print(f)
+    contents = generate_go_file(defuns)
+    with open(pimacs_base.joinpath("elisp/emacs_autogen.go"), "w") as f:
+        f.write(contents)
 
 
 if __name__ == "__main__":
